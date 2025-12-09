ae1d05bdd60abb331ac56ccc443758921b484e9e
manaswi <pithanimanaswi@gmail.com>
2025-12-09 12:08:15 +0530

    updated

--- Components/Products.jsx
+++ Components/Products.jsx
@@ -1,26 +1,22 @@
-import React, {useEffect,useState} from 'react'
-import Product from './Product';
+import React, { useEffect, useState } from "react";
+import Product from "./Product";
 
 function Products() {
-var [products,setProducts]=useState();
-   useEffect(()=>{
-     fetch("https://dummyjson.com/products")
-    .then((res)=>res.json())
-    .then((data)=>{
-        console.log(data);
-        setProducts(data.products)
-    })
-   },[]);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products));
+  }, []);
+
   return (
-    <div className='border boredr-2 border-info  p-2 text-center' style={{margin:"auto"}}>
-     <h1>Products</h1>
- <ul className='d-flex flex-wrap gap-4  ' >
-     {products?.map((prod)=>{
-        return <Product prod={prod}></Product>;
-           })}
-           </ul>
+    <div className="d-flex flex-wrap gap-3">
+      {products.map((p) => (
+        <Product key={p.id} prod={p} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
